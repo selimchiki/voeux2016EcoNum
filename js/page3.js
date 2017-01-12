2207ed181857ee79853fbda697ede02a6be207a9
selim chiki <selimchiki@yahoo.fr>
2017-01-12 12:29:37 +0100

    ajout du svg de mica dans la page index, ajout du svg popschool dans la vidéo avec effet jquery, et mise en page via le css pour les mediaquery

--- js/page3.js
+++ js/page3.js
@@ -1,6 +1,15 @@
-
 $(document).ready(function() {
-$("video").bind("ended", function() {
-   location.href = "page4.html";
- });  	
+	$('.footer').hide();
+	$('svg').hide();
+	function showSVG(){
+		$('svg').show();
+		$('.footer').show();
+		$('.footer').css('margin-top','300px');
+		$('.footer').css('text-align','center');
+		$('svg').animate({
+			left: "30%",
+			width : "40%"
+		}, 1500);
+	}
+	setTimeout(showSVG, 5500);
 });
