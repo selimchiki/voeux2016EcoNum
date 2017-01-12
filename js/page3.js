240bf6b7fd2d7c80138379ccb6ea0d6621e3bc76
selim chiki <selimchiki@yahoo.fr>
2017-01-12 17:36:26 +0100

    modif media

--- js/page3.js
+++ js/page3.js
@@ -1,24 +1,24 @@
 $(document).ready(function() {
-	if (window.matchMedia("(min-width: 420px)").matches) {
+	if (window.matchMedia("(max-width: 420px)").matches) {
 		$('.footer').hide();
 		$('svg').hide();
-		function showSVG(){
+        function nouvelPage() {
+			$('html').load("page4.html");
+		}
+		setTimeout(nouvelPage, 33000);
+	} else {
+        $('.footer').hide();
+		$('svg').hide();
+		function showSVG() {
 			$('svg').show();
 			$('.footer').show();
-			$('.footer').css('margin-top','300px');
-			$('.footer').css('text-align','center');
+			$('.footer').css('margin-top', '300px');
+			$('.footer').css('text-align', 'center');
 			$('svg').animate({
 				left: "30%",
 				width : "40%"
 			}, 1500);
 		}
         setTimeout(showSVG, 5500);
-	} else {
-		$('.footer').hide();
-		$('svg').hide();
-		function nouvelPage(){
-			$('html').load("page4.html");
-		}
-		setTimeout(nouvelPage,33000);
 	}
 });
